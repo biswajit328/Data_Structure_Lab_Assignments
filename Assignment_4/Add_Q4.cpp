e304f1495e092a12381b0879bd431bc0b48da4dc
Biswajit Mandal <biswajit328328@gmail.com>
2025-11-27 19:13:46 +0530

    Add files via upload

---
+++ Assignment_4/Add_Q4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int countStudents(vector<int>& students, vector<int>& sandwiches) {
+    queue<int> q;
+    for (int s : students) q.push(s);
+    int i = 0, count = 0;
+
+    while (!q.empty() && count < q.size()) {
+        if (q.front() == sandwiches[i]) {
+            q.pop();
+            i++;
+            count = 0;
+        } else {
+            q.push(q.front());
+            q.pop();
+            count++;
+        }
+    }
+    return q.size();
+}
+
+int main() {
+    vector<int> students = {1,1,0,0};
+    vector<int> sandwiches = {0,1,0,1};
+    cout << "Students unable to eat: " << countStudents(students, sandwiches);
+}
